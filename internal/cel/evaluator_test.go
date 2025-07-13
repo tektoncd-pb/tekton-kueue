e04ffb85b3a4079e8aae6441a3c7128bb7ed4aa4
Gal Ben Haim <gbenhaim@redhat.com>
2025-07-13 08:05:08 +0300

    Add unit test for custom CEL variables
    Signed-off-by: Gal Ben Haim <gbenhaim@redhat.com>

--- internal/cel/evaluator_test.go
+++ internal/cel/evaluator_test.go
@@ -76,6 +76,160 @@ func TestCompiledProgram_Evaluate_TypeSafety(t *testing.T) {
 			expectErr:   true,
 			errMsg:      "mutation value cannot be empty",
 		},
+		{
+			name:        "plrNamespace variable",
+			expression:  `annotation("namespace", plrNamespace)`,
+			pipelineRun: pipelineRun,
+			expected: []MutationRequest{
+				{Type: MutationTypeAnnotation, Key: "namespace", Value: "test-namespace"},
+			},
+		},
+		{
+			name:       "pacEventType variable with existing label",
+			expression: `annotation("event-type", pacEventType)`,
+			pipelineRun: &tekv1.PipelineRun{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-pipeline",
+					Namespace: "test-namespace",
+					Labels: map[string]string{
+						"pipelinesascode.tekton.dev/event-type": "push",
+					},
+				},
+				Spec: tekv1.PipelineRunSpec{
+					PipelineRef: &tekv1.PipelineRef{
+						Name: "test-pipeline",
+					},
+				},
+			},
+			expected: []MutationRequest{
+				{Type: MutationTypeAnnotation, Key: "event-type", Value: "push"},
+			},
+		},
+		{
+			name:        "pacEventType variable with missing label",
+			expression:  `annotation("event-type", pacEventType)`,
+			pipelineRun: pipelineRun,
+			expectErr:   true,
+			errMsg:      "mutation value cannot be empty",
+		},
+		{
+			name:       "pacTestEventType variable with existing label",
+			expression: `annotation("test-event-type", pacTestEventType)`,
+			pipelineRun: &tekv1.PipelineRun{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-pipeline",
+					Namespace: "test-namespace",
+					Labels: map[string]string{
+						"pac.test.appstudio.openshift.io/event-type": "unit-test",
+					},
+				},
+				Spec: tekv1.PipelineRunSpec{
+					PipelineRef: &tekv1.PipelineRef{
+						Name: "test-pipeline",
+					},
+				},
+			},
+			expected: []MutationRequest{
+				{Type: MutationTypeAnnotation, Key: "test-event-type", Value: "unit-test"},
+			},
+		},
+		{
+			name:        "pacTestEventType variable with missing label",
+			expression:  `annotation("test-event-type", pacTestEventType)`,
+			pipelineRun: pipelineRun,
+			expectErr:   true,
+			errMsg:      "mutation value cannot be empty",
+		},
+		{
+			name:        "safe pacEventType usage with conditional",
+			expression:  `pacEventType != "" ? annotation("event-type", pacEventType) : annotation("event-type", "unknown")`,
+			pipelineRun: pipelineRun,
+			expected: []MutationRequest{
+				{Type: MutationTypeAnnotation, Key: "event-type", Value: "unknown"},
+			},
+		},
+		{
+			name:        "safe pacTestEventType usage with conditional",
+			expression:  `pacTestEventType != "" ? annotation("test-event-type", pacTestEventType) : annotation("test-event-type", "none")`,
+			pipelineRun: pipelineRun,
+			expected: []MutationRequest{
+				{Type: MutationTypeAnnotation, Key: "test-event-type", Value: "none"},
+			},
+		},
+		{
+			name:        "conditional expression with plrNamespace",
+			expression:  `plrNamespace == "test-namespace" ? label("env", "testing") : label("env", "other")`,
+			pipelineRun: pipelineRun,
+			expected: []MutationRequest{
+				{Type: MutationTypeLabel, Key: "env", Value: "testing"},
+			},
+		},
+		{
+			name:       "conditional expression with pacEventType",
+			expression: `pacEventType == "push" ? label("trigger", "push-event") : label("trigger", "other-event")`,
+			pipelineRun: &tekv1.PipelineRun{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-pipeline",
+					Namespace: "test-namespace",
+					Labels: map[string]string{
+						"pipelinesascode.tekton.dev/event-type": "push",
+					},
+				},
+				Spec: tekv1.PipelineRunSpec{
+					PipelineRef: &tekv1.PipelineRef{
+						Name: "test-pipeline",
+					},
+				},
+			},
+			expected: []MutationRequest{
+				{Type: MutationTypeLabel, Key: "trigger", Value: "push-event"},
+			},
+		},
+		{
+			name:       "conditional expression with pacTestEventType",
+			expression: `pacTestEventType != "" ? label("test-type", pacTestEventType) : label("test-type", "none")`,
+			pipelineRun: &tekv1.PipelineRun{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-pipeline",
+					Namespace: "test-namespace",
+					Labels: map[string]string{
+						"pac.test.appstudio.openshift.io/event-type": "integration-test",
+					},
+				},
+				Spec: tekv1.PipelineRunSpec{
+					PipelineRef: &tekv1.PipelineRef{
+						Name: "test-pipeline",
+					},
+				},
+			},
+			expected: []MutationRequest{
+				{Type: MutationTypeLabel, Key: "test-type", Value: "integration-test"},
+			},
+		},
+		{
+			name:       "multiple variables in one expression",
+			expression: `[annotation("namespace", plrNamespace), annotation("event", pacEventType), annotation("test-event", pacTestEventType)]`,
+			pipelineRun: &tekv1.PipelineRun{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-pipeline",
+					Namespace: "test-namespace",
+					Labels: map[string]string{
+						"pipelinesascode.tekton.dev/event-type":      "pull_request",
+						"pac.test.appstudio.openshift.io/event-type": "e2e-test",
+					},
+				},
+				Spec: tekv1.PipelineRunSpec{
+					PipelineRef: &tekv1.PipelineRef{
+						Name: "test-pipeline",
+					},
+				},
+			},
+			expected: []MutationRequest{
+				{Type: MutationTypeAnnotation, Key: "namespace", Value: "test-namespace"},
+				{Type: MutationTypeAnnotation, Key: "event", Value: "pull_request"},
+				{Type: MutationTypeAnnotation, Key: "test-event", Value: "e2e-test"},
+			},
+		},
 	}
 
 	for _, tt := range tests {
